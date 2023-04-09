b8a52a65f414e3a9e9ec775c17dd135b21c2b917
Constantinos Sergiou <coustas.serge@gmail.com>
2023-04-09 17:56:20 +0300

    add services

---
+++ src/Services/MyService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Constantinos\SecurityHeadersBundleServices\Services;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class MyService implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function myMethod()
+    {   
+        return 'hello world';
+        // Your method code here
+    }
+}
